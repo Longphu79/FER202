dd5df464847d318bda810d691c6390412147da3d
Longphu79 <lelongphu2004@gmail.com>
2025-02-20 14:42:45 +0700

    file code

---
+++ react_hook/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Counter from './components/Counter';
+import ProductList from './components/ProductList';
+function App() {
+  const [name, setName] = useState('Adam');
+  const [age, setAge] = useState(35);
+
+  return (
+    <div className="App">
+      <Counter/>
+      <input 
+        type="text" 
+        value={name} 
+        onChange={(e) => {setName(e.target.value);
+          console.log(e.target.value)
+        }} 
+      />
+      <p>My name is {name}</p>
+
+      <input 
+        type="number" 
+        value={age} 
+        onChange={(e) => setAge(parseInt(e.target.value, 10))} 
+      />
+      <p>My age is {age}</p>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
